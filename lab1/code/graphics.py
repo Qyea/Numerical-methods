d807ec2596f5ba5184219ea7991a6b632c714172
Qyea <kreshess@mail.ru>
2024-02-19 21:30:05 +0300

    some changes

--- lab1/code/graphics.py
+++ lab1/code/graphics.py
@@ -1,37 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-def print_statistics(max_error_equidistant, max_error_chebyshev, n_values, function_name):
-    file_name = function_name + ".txt"
-    print("n    Max Error (Equidistant)    Max Error (Chebyshev)")
-    for i in range(len(n_values)):
-        print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}")
-        
-    with open(file_name, "w") as file:
-        print("n    Max Error (Equidistant)    Max Error (Chebyshev)", file=file)
-        for i in range(len(n_values)):
-            print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}", file=file)
-        
-
-def calculate_statistics(f, n_values):
-    for n in n_values:
-    # Равноотстоящие узлы для интерполяции f2(x)
-        equidistant_nodes = choose_equidistant_nodes(n, -3, 3)
-        equidistant_coefficients = newton_interpolation(equidistant_nodes, f(equidistant_nodes))
-        equidistant_interpolated = np.polyval(equidistant_coefficients[::-1], x)
-        max_error_equidistant = np.max(np.abs(equidistant_interpolated - f(x)))
-        max_errors_equidistant.append(max_error_equidistant)
-
-        # Чебышевские узлы для интерполяции f2(x)
-        chebyshev_nodes = choose_chebyshev_nodes(n, -3, 3)
-        chebyshev_coefficients = newton_interpolation(chebyshev_nodes, f(chebyshev_nodes))
-        chebyshev_interpolated = np.polyval(chebyshev_coefficients[::-1], x)
-        max_error_chebyshev = np.max(np.abs(chebyshev_interpolated - f(x)))
-        max_errors_chebyshev.append(max_error_chebyshev)
-        function_name = f.__name__
-    
-    print_statistics(max_error_equidistant, max_error_chebyshev, n_values, function_name)
+import math
     
 # Определение функции f1(x) = sin(cos(x))
 def f1(x):
@@ -43,7 +12,8 @@ def f2(x):
 
 # Выбор узлов (равноотстоящие)
 def choose_equidistant_nodes(n, a, b):
-    return np.linspace(a, b, n + 1)
+    # step = (b-a)/n => for(i){nodesArray+= a+step*i;}
+    return np.linspace(a, b, n + 1) 
 
 # (чебышёвские)
 def choose_chebyshev_nodes(n, a, b):
@@ -56,21 +26,21 @@ def newton_interpolation(x, y):
     n = len(x) - 1
     coefficients = y.copy()
 
-    for j in range(1, n + 1):
-        for k in range(1, n - j + 1):
-            i = k + j
-            coefficients[i] = (coefficients[i] - coefficients[i - 1]) / (x[i] - x[i - j])
+    for i in range(1, n + 1):
+        for j in range(0, n + 1 - i):
+            coefficients[j+i] = (coefficients[j+i] - coefficients[j+i-1]) / (x[j+i] - x[j])
 
     return coefficients
 
 
 def draw_graphics(n_values, f):
     for i in range(len(n_values)):
+        # Равноотстоящие узлы для интерполяции f(x)
         equidistant_nodes = choose_equidistant_nodes(n_values[i], -3, 3)
         equidistant_coefficients = newton_interpolation(equidistant_nodes, f(equidistant_nodes))
         equidistant_interpolated = np.polyval(equidistant_coefficients[::-1], x)
 
-        # Чебышевские узлы для интерполяции f1(x)
+        # Чебышевские узлы для интерполяции f(x)
         chebyshev_nodes = choose_chebyshev_nodes(n_values[i], -3, 3)
         chebyshev_coefficients = newton_interpolation(chebyshev_nodes, f(chebyshev_nodes))
         chebyshev_interpolated = np.polyval(chebyshev_coefficients[::-1], x)
@@ -85,14 +55,45 @@ def draw_graphics(n_values, f):
         plt.legend()
 
         plt.subplot(122)
-        plt.plot(x, f1(x), label=function_name)
+        plt.plot(x, f(x), label=function_name)
         plt.plot(x, chebyshev_interpolated, label='C1,n (Chebyshev)')
         plt.title('Interpolation of '+ function_name+ ' - Chebyshev Nodes')
         plt.legend()
 
+def print_statistics(max_errors_equidistant, max_errors_chebyshev, n_values, function_name):
+    file_name = function_name + ".txt"
+    print("n    Max Error (Equidistant)    Max Error (Chebyshev)")
+    for i in range(len(n_values)):
+        print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}")
+        
+    with open(file_name, "w") as file:
+        print("n    Max Error (Equidistant)    Max Error (Chebyshev)", file=file)
+        for i in range(len(n_values)):
+            print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}", file=file)
+        
+
+def calculate_statistics(f, n_values):
+    max_errors_equidistant = []
+    max_errors_chebyshev = []
+    for n in n_values:
+    # Равноотстоящие узлы для интерполяции f(x)
+        equidistant_nodes = choose_equidistant_nodes(n, -3, 3)
+        equidistant_coefficients = newton_interpolation(equidistant_nodes, f(equidistant_nodes))
+        equidistant_interpolated = np.polyval(equidistant_coefficients[::-1], x)
+        max_error_equidistant = np.max(np.abs(equidistant_interpolated - f(x)))
+        max_errors_equidistant.append(max_error_equidistant*((3-(-3))**(n+1))/(math.factorial(n+1)*(2**(2*n + 1))))
+
+        # Чебышевские узлы для интерполяции f(x)
+        chebyshev_nodes = choose_chebyshev_nodes(n, -3, 3)
+        chebyshev_coefficients = newton_interpolation(chebyshev_nodes, f(chebyshev_nodes))
+        chebyshev_interpolated = np.polyval(chebyshev_coefficients[::-1], x)
+        max_error_chebyshev = np.max(np.abs(chebyshev_interpolated - f(x)))
+        max_errors_chebyshev.append(max_error_chebyshev*((3-(-3))**(n+1))/(math.factorial(n+1)*(2**(2*n + 1))))
+        function_name = f.__name__
+    print_statistics(max_errors_equidistant, max_errors_chebyshev, n_values, function_name)    
+    
 # Генерация точек на оси x для построения графиков
 x = np.linspace(-3, 3, 100)
-
 
 n_values = [3, 10, 20]  # Значения n для интерполяции
 
@@ -107,37 +108,6 @@ plt.show()
 
 # Создание таблицы погрешностей
 n_values = range(3, 31)  # Значения n для интерполяции
-max_errors_equidistant = []
-max_errors_chebyshev = []
 
 calculate_statistics(f1, n_values)
 calculate_statistics(f2, n_values)
-
-
-"""for n in n_values:
-    # Равноотстоящие узлы для интерполяции f2(x)
-    equidistant_nodes = choose_equidistant_nodes(n, -3, 3)
-    equidistant_coefficients = newton_interpolation(equidistant_nodes, f2(equidistant_nodes))
-    equidistant_interpolated = np.polyval(equidistant_coefficients[::-1], x)
-    max_error_equidistant = np.max(np.abs(equidistant_interpolated - f2(x)))
-    max_errors_equidistant.append(max_error_equidistant)
-
-    # Чебышевские узлы для интерполяции f2(x)
-    chebyshev_nodes = choose_chebyshev_nodes(n, -3, 3)
-    chebyshev_coefficients = newton_interpolation(chebyshev_nodes, f2(chebyshev_nodes))
-    chebyshev_interpolated = np.polyval(chebyshev_coefficients[::-1], x)
-    max_error_chebyshev = np.max(np.abs(chebyshev_interpolated - f2(x)))
-    max_errors_chebyshev.append(max_error_chebyshev)
-
-# Вывод таблицы погрешностей
-print("n    Max Error (Equidistant)    Max Error (Chebyshev)")
-for i in range(len(n_values)):
-    print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}")
-    
-with open("table.txt", "w") as file:
-    print("n    Max Error (Equidistant)    Max Error (Chebyshev)", file=file)
-    for i in range(len(n_values)):
-        print(f"{n_values[i]:2d}   {max_errors_equidistant[i]:.6f}     {max_errors_chebyshev[i]:.6f}", file=file)"""
-        
-
-        
--- lab1/code/tets.py
+++ lab1/code/tets.py
@@ -1,17 +1,185 @@
-import numpy as np
-from scipy.optimize import curve_fit
+from typing import List
+from math import fabs
+from numpy import arange
+import matplotlib.pyplot as plt
 
-def polynomial_func(x, a, c):
-    return a * x**2 + x + c
+import math
 
-# Задаем данные
-x_data = np.linspace(-1, 1, 100)
-y_data = np.random.normal(0, 0.1, size=100)  # Добавляем шум к данным
+class MyRange:
+    def __init__(self, a, b):
+        if a > b:
+            raise ValueError("Invalid range")
+        
+        self.A = a
+        self.B = b
+    
+    def GetEquidistantKnots(self, n):
+        knots = []
+        step = (self.B - self.A) / n
 
-# Выполняем аппроксимацию с использованием метода наименьших квадратов
-popt, pcov = curve_fit(polynomial_func, x_data, y_data)
+        for i in range(n + 1):
+            knots.append(self.A + step * i)
 
-# Получаем коэффициенты многочлена
-a_opt, b_opt, c_opt = popt
+        return knots
+    
+    def GetChebyshevKnots(self, n):
+        knots = []
 
-print(f"Наименьше уклоняющийся от нуля многочлен: {a_opt}x^2 + {b_opt}x + {c_opt}")
+        for i in range(n + 1):
+            knots.append((self.A + self.B) / 2 + ((self.B - self.A) / 2) * math.cos(math.pi * (2 * i + 1) / (2 * (n + 1))))
+
+        return knots
+    
+    def GetPoints(self):
+        points = []
+
+        for i in range(101):
+            points.append(self.A + i * (self.B - self.A) / 100)
+
+        return points
+
+class Form1:
+    def __init__(self):
+        self._functions = MyRange(-3, 3)
+
+    def CalculatePn(self, n: int, type: int):
+        name = ""
+        if type == 1:
+            name = "P1"
+        elif type == 2:
+            name = "P2"
+        else:
+            raise NotSupportedException()
+
+        result = self.CalculatePolynomial(type, n, 1)
+        table = self.GetErrorTable(type, name, 1)
+
+        stringBuilder = ""
+        for line in table:
+            stringBuilder += line + "\n"
+
+        Pn.Text = stringBuilder
+
+        line1 = LineSeries()
+        line1.Title = name
+        line1.Color = 'blue'
+        line1.StrokeThickness = 1
+        line1.MarkerSize = 2
+        line1.MarkerType = 'o'
+
+        for item in result.First:
+            line1.Points.append(DataPoint(item.First, item.Second))
+
+        return line1
+
+    def CalculateCn(self, n: int, type: int):
+        name = ""
+        if type == 1:
+            name = "C1"
+        elif type == 2:
+            name = "C2"
+        else:
+            raise NotSupportedException()
+
+        result = self.CalculatePolynomial(type, n, 2)
+        table = self.GetErrorTable(type, name, 2)
+
+        stringBuilder = ""
+        for line in table:
+            stringBuilder += line + "\n"
+
+        Cn.Text = stringBuilder
+
+        line1 = LineSeries()
+        line1.Title = name
+        line1.Color = 'red'
+        line1.StrokeThickness = 1
+        line1.MarkerSize = 2
+        line1.MarkerType = 'o'
+
+        for item in result.First:
+            line1.Points.append(DataPoint(item.First, item.Second))
+
+        return line1
+
+    def PrintReport(self, n: int, type: int):
+        pn = self.CalculatePn(n, type)
+        cn = self.CalculateCn(n, type)
+
+        function = None
+        if type == 1:
+            function = self._functions.F1
+        elif type == 2:
+            function = self._functions.F2
+        else:
+            raise NotSupportedException()
+
+        myModel = PlotModel()
+        myModel.Title = ""
+
+        legend = Legend()
+        legend.LegendTitle = ""
+        legend.LegendPosition = LegendPosition.RightTop
+        myModel.Legends.append(legend)
+
+        myModel.Series.append(FunctionSeries(function, self._functions.Range.A, self._functions.Range.B, 0.001))
+        myModel.Series.append(pn)
+        myModel.Series.append(cn)
+        plotView1.Model = myModel
+
+    def Button1_Click(self, sender, e):
+        if f1.Checked:
+            if int.TryParse(textBox1.Text, out var n):
+                PrintReport(n, 1)
+            else:
+                textBox1.Text = "Wrong number!"
+
+        if f2.Checked:
+            if int.TryParse(textBox1.Text, out var n):
+                PrintReport(n, 2)
+            else:
+                textBox1.Text = "Wrong number!"
+
+    def F1_CheckedChanged(self, sender, e):
+        if f1.Checked:
+            f2.Checked = False
+        else:
+            f2.Checked = True
+
+    def F2_CheckedChanged(self, sender, e):
+        if f2.Checked:
+            f1.Checked = False
+        else:
+            f1.Checked = True
+
+    def GetErrorTable(self, type: int, polynomial: str, knotsType: int) -> List[str]:
+        result = []
+
+        for i in range(3, 31):
+            pair = self.CalculatePolynomial(type, i, knotsType)
+
+            max_val = 0.0
+
+            for j in range(len(pair.First)):
+                max_val = max(fabs(pair.First[j].Second - pair.Second[j]), max_val)
+
+            result.append(f"n = {i}: max|{polynomial} - f{type}| = {max_val}")
+
+        return result
+
+    def CalculatePolynomial(self, type: int, n: int, knotsType: int):
+        knots = []
+        if knotsType == 1:
+            knots = self._functions.Range.GetEquidistantKnots(n)
+        elif knotsType == 2:
+            knots = self._functions.Range.GetChebyshevKnots(n)
+        else:
+            raise NotSupportedException()
+
+        function = None
+        if type == 1:
+            function = self._functions.F1
+        elif type == 2:
+            function = self._functions.F2
+        else:
+            raise NotSupportedException()
