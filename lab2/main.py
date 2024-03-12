da499a9c7c6ed77d7b574e33db8f5109b90fd5bf
Qyea <kreshess@mail.ru>
2024-03-12 23:13:18 +0300

    final version

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from decimal import Decimal
 
 # Задание интервала
 a = -3
@@ -8,125 +9,98 @@ b = 3
 # Задание количества узлов
 N = 15
 
-# Граничные условия
-f_prime_a = 0.077432
-f_prime_b= -0.077432
+h = (b-a)/N
 
 # Задание функции f(x) = sin(cos(x))
 def f(x):
     return np.sin(np.cos(x))
 
-# Массив значений функции f(x) в точках 
-def f_points(build_points, n=15):
-    return [f(build_points[i]) for i in range(n + 1)]
+def df(x):
+    return (-np.cos(np.cos(x))*np.sin(x))
 
-# Выбор узлов (равноотстоящие)
-def equidistant_nodes(n=15):
-    return np.linspace(a, b, n+1)
-
-# Массив значений hi
-def h_value(n=15):
-    points = equidistant_nodes()
-    h_values = np.zeros(n+1)
-    for i in range(1, n+1): 
-        h_values[i] = points[i] - points[i - 1]
-        
-    return h_values
-
-# Метод прогонки
 def solve_tridiagonal(a, b, c, f):
-    # Прямой ход (прогонка)
-    alpha = [-1, b[0] / c[0]]
-    beta = [-1, f[0] / c[0]]
-    for i in range(1, N):
-        alpha.append(b[i] / (c[i] - a[i] * alpha[i]))
-    for i in range(1, N+1):
-        beta.append((f[i] + a[i] * beta[i]) / (c[i] - a[i] * alpha[i]))
+    n = len(f)
+    alpha = [0.0] * n
+    beta = [0.0] * n
 
-    x = [beta[N+1]]
+    alpha[0] = -b[0] / c[0]
+    beta[0] = f[0] / c[0]
+
+    for i in range(1, n-1):
+        alpha[i] = -b[i] / (c[i] + a[i-1] * alpha[i - 1])
+    for i in range(1, n):
+        beta[i] = (f[i] - a[i-1] * beta[i - 1]) / (c[i] + a[i-1] * alpha[i - 1])
+
     # Обратный ход (обратная прогонка)
-    for i in range(N-1, -1, -1):
-        x.append(alpha[i + 1] * x[-1] + beta[i + 1])
+    x = [0.0] * n
+    x[n - 1] = beta[n - 1]
+    for i in range(n - 2, -1, -1):
+        x[i] = alpha[i] * x[i + 1] + beta[i]
 
-    return x[::-1]
-
-# Вычисление моментов
-def find_M():
-    points = equidistant_nodes()
-    fp = f_points(points)
-    h_values = h_value()
-    c = [h_values[1] / 3]
+    return x
     
-    for i in range(1, N):
-        c.append((h_values[i] + h_values[i+1]) / 3)
-    c.append(1)
+def compute_S3(x, M, f):
+    S3_x = [lambda i: lambda x: ( M[i - 1] * ((x[i] - x) ** 3) / (6 * h) + M[i] * ((x - x[i-1]) ** 3) / (6 * h) + (f[i - 1] - (h * h) * M[i - 1] / 6) * (x[i] - x) / h + (f[i] - (h * h) * M[i] / 6) * (x - x[i-1]) / h) for i in range(1, N + 1)]
+    return S3_x
 
-    a = [-1]
-    for i in range(1, N):
-        a.append(-1 * h_values[i] / 6)
-    a.append(0)
-
-    b = []
-    for i in range(1, N):
-        b.append(-1 * h_values[i+1] / 6)
-    b.append(-1)
-
-    f = [(fp[1] - fp[0]) / h_values[1] - f_prime_a]
-    
-    for i in range(1, N):
-        f.append((fp[i + 1] - fp[i]) / h_values[i+1] - (fp[i] - fp[i - 1]) / h_values[i])
-    f.append(f_prime_b)
-    M = np.ones(N+1)
-    M = solve_tridiagonal(a, b, c, f)
-    return M
-
-
-def get_index(x):
-    points = equidistant_nodes()
-    for i in range(1, N+1):
-        if points[i - 1] <= x <= points[i]:
-            return i
-
-
-def P(h_value, i, x, nodes, M, f):
-    hi = h_value
-    result =  M[i - 1] * ((nodes[i] - x) ** 3) / (hi*6) + M[i] * ((x - nodes[i-1]) ** 3)  / (hi*6)
-    # Ai*(x_i-x)/h_i +Bi*(x-x_i)/h_i
-    result += (f[i - 1] - (hi ** 2 / 6) * M[i - 1]) * ((nodes[i] - x) / hi) + (f[i] - (hi ** 2 / 6 )* M[i]) * ((x - nodes[i - 1]) / hi) 
-    return result
-
-# Функция вычисления сплайна
-def spline():
-    build_points = equidistant_nodes(100)
-    points = equidistant_nodes()
-    M = find_M()
-    h_values = h_value()
-    f = f_points(points)
-    s = []
-    for x in build_points:
-        i = get_index(x)
-        s.append(P(h_values[i], i, x, points, M, f))
+def createS3(i, x_nodes, M, f):
+    def s(x): 
+        return (M[i - 1] * ((x_nodes[i] - x) ** 3) / (6 * h) + M[i] * ((x - x_nodes[i-1]) ** 3) / (6 * h) + (f[i - 1] - (h * h) * M[i - 1] / 6) * (x_nodes[i] - x) / h + (f[i] - (h * h) * M[i] / 6) * (x - x_nodes[i-1]) / h)
     return s
 
-# Вычисление ошибок
-def error():
-    return np.abs(np.array(f_points(equidistant_nodes(100), 100)) - np.array(spline()))
 
-# Вычисление узлов
-x_nodes = equidistant_nodes()
-x_interp = equidistant_nodes(100)
-
-# Вычисление значений функции f(x)
+# Вычисление узлов и значений функции в узлах
+x_nodes = np.linspace(a, b, N+1)
 f_values = f(x_nodes)
-f_values_interp = f(x_interp) # в узлах x*_i
 
-# Вычисление сплайна
-spline_values = spline()
+
+f_vector = []
+f_vector.append((f_values[1] - f_values[0]) / h - df(a)) 
+for i in range(1,N): 
+    f_vector.append((f_values[i+1] - f_values[i])/h - (f_values[i]-f_values[i-1])/h)
+f_vector.append(df(b) - (f_values[N] - f_values[N-1]) / h)
+
+upper_diagonal = []
+middle_diagonal = []
+lower_diagonal = []
+
+middle_diagonal.append(h / 3)
+upper_diagonal.append(h / 6) 
+
+for i in range(1, N):
+    upper_diagonal.append(h / 6)
+    middle_diagonal.append((2*h) / 3)
+    lower_diagonal.append(h / 6)
+
+lower_diagonal.append(h / 6)
+middle_diagonal.append(h / 3)
+
+
+M_values = solve_tridiagonal(lower_diagonal, upper_diagonal, middle_diagonal, f_vector)
+
+S3_x = []
+
+for i in range(1, N+1):
+    S3_x.append(createS3(i, x_nodes, M_values, f_values))
+
+def S(x):
+    i  = int((x - a)/h)
+    if i == N:
+        i = (N-1)
+    return S3_x[i](x)
+
+def error():
+    return np.abs(np.array(f_values_interp) - np.array(S3x))
+
+x_interp = np.linspace(a, b, 101)
+f_values_interp = f(x_interp)
+
+S3x = [S(x) for x in x_interp]
 
 # Построение графиков
 plt.figure(figsize=(10, 6))
 plt.plot(x_interp, f_values_interp, label='f(x)')
-plt.plot(x_interp, spline_values, color='orange', label='S3(x)')
+plt.plot(x_interp, S3x, color='orange', label='S3(x)')
 plt.scatter(x_nodes, f_values, color='red', label='Узлы')
 plt.xlabel('x')
 plt.ylabel('y')
@@ -134,12 +108,11 @@ plt.title('Интерполяционный кубический сплайн')
 plt.legend()
 plt.show()
 
-max_error = (max(error()))
-print(f'Max погрешность: {max_error}')
+max_error = np.max(np.abs(S3x - f_values_interp))
+print(max_error)
 
-# Построение графика погрешности
 plt.figure(figsize=(10, 4))
-plt.plot(equidistant_nodes(100), error(), label='Погрешность')
+plt.plot(x_interp, error(), label='Погрешность')
 plt.xlabel('x')
 plt.ylabel('Погрешность')
 plt.title('Погрешность интерполирования кубическим сплайном')
